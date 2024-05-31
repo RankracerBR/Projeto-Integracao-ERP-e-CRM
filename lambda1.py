65dc70694bc5299cb2e1e085d0c81e3f05a1de0a
Augusto Pontes <augustopontes010@gmail.com>
2024-05-31 02:27:12 -0300

    Alterações nos lambdas

--- lambda1.py
+++ lambda1.py
@@ -1,25 +1,62 @@
+import os
+import logging
 import json
 import boto3
 
-# Read the mocked file
-with open('erp_data.json', 'r') as file:
-    data = json.load(file)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
-# Modify the data if needed
-# For example, let's change the status of the last entry to "finished"
-data[-1]['status'] = 'finished'
+def replace_null_with_none(data):
+    if isinstance(data, dict):
+        return {k: replace_null_with_none(v) for k, v in data.items()}
+    elif isinstance(data, list):
+        return [replace_null_with_none(item) for item in data]
+    elif data is None:
+        return "Nenhum"
+    else:
+        return data
 
-# Convert the modified data back to JSON string
-modified_data = json.dumps(data)
+def lambda_handler(event, context):
+    logger.info(event)
 
-# Initialize Boto3 S3 client
-s3 = boto3.client('s3')
+    bucket_name = os.environ.get("BUCKET_NAME", None)
+    try:
+        if event and bucket_name:
+            s3 = boto3.client("s3")
 
-# Specify bucket name and object key
-bucket_name = 'test-lambda-api-project'
-object_key = 'modified_data.json'
+            path_parameters = event.get("pathParameters", {})
+            query_parameters = event.get("queryStringParameters", {})
 
-# Upload the modified data to S3 bucket
-s3.put_object(Bucket=bucket_name, Key=object_key, Body=modified_data)
+            user_name = path_parameters.get("username")
+            file_name = query_parameters.get("filename")
 
-print("Modified data has been uploaded to S3 bucket.")
+            if not user_name or not file_name:
+                raise ValueError("Missing 'username' or 'filename' in the request parameters")
+
+            if not file_name.endswith('.json'):
+                raise ValueError("O arquivo deve ter a extensão .json")
+
+            # Baixar o arquivo JSON do S3
+            obj = s3.get_object(Bucket=bucket_name, Key=f"{user_name}/{file_name}")
+            data = json.loads(obj['Body'].read())
+
+            # Modificar os dados
+            data = replace_null_with_none(data)
+            data[-1]['status'] = 'finished'
+
+            # Converter os dados modificados de volta para uma string JSON com formatação
+            modified_data = json.dumps(data, indent=4, sort_keys=True)
+
+            # Enviar os dados modificados de volta para o S3
+            s3.put_object(Bucket=bucket_name, Key=f"{user_name}/{file_name}", Body=modified_data)
+
+            return {
+                "statusCode": 200,
+                "headers": {"Content-Type": "application/json"},
+                "body": json.dumps({"message": "Modified data has been uploaded to S3 bucket."}),
+            }
+    except Exception as e:
+        logger.error("Exception occurred", exc_info=True)
+        return {"statusCode": 500, "body": json.dumps("Error processing the request!")}
+
+    return {"statusCode": 500, "body": json.dumps("Error processing the request!")}
--- lambda2.py
+++ lambda2.py
@@ -1,20 +1,88 @@
 import boto3
 import json
+import urllib.request
+import urllib.error
+import logging
 
 s3_client = boto3.client("s3")
 S3_BUCKET = 'test-lambda-api-project'
 
+# Configuração do logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def get_crm_endpoint(json_data):
+    try:
+        # Extrai a URL do servidor do JSON
+        server_url = json_data.get("servers", [{}])[0].get("url")
+        if server_url:
+            return server_url
+        else:
+            logger.error("URL do servidor não encontrada no arquivo JSON.")
+            return None
+    except Exception as e:
+        logger.error(f"Erro ao obter a URL do servidor do arquivo JSON: {e}")
+        return None
+
+def send_to_crm(data, endpoint):
+    try:
+        # Converte os dados em formato JSON
+        data_json = json.dumps(data).encode('utf-8')
+        
+        # Realiza a requisição POST para o CRM
+        req = urllib.request.Request(endpoint, data=data_json, headers={'Content-Type': 'application/json'})
+        with urllib.request.urlopen(req) as response:
+            if response.getcode() == 200:
+                logger.info("Dados enviados com sucesso para o CRM.")
+                return True
+            else:
+                logger.error(f"Falha ao enviar dados para o CRM. Código de status: {response.getcode()}")
+                return False
+    except urllib.error.HTTPError as e:
+        logger.error(f"Falha ao enviar dados para o CRM. Código de erro: {e.code}")
+        return False
+    except urllib.error.URLError as e:
+        logger.error(f"Erro de URL ao enviar dados para o CRM: {e.reason}")
+        return False
+    except Exception as e:
+        logger.error(f"Erro ao enviar dados para o CRM: {e}")
+        return False
+
 def lambda_handler(event, context):
-    object_key = "modified_data.json"  # replace object key
+    object_key = "Augusto/modified_data2.json"  # substitua pelo objeto correto
     try:
         file_content = s3_client.get_object(Bucket=S3_BUCKET, Key=object_key)["Body"].read().decode('utf-8')
-        print(file_content)
-        return {
-            'statusCode': 200,
-            'body': file_content
-        }
+        logger.info("Conteúdo do arquivo do S3: %s", file_content)
+        
+        # Convertendo o conteúdo do arquivo JSON para um dicionário Python
+        data = json.loads(file_content)
+        
+        # Carrega o arquivo JSON do CRM
+        with open('crm_swagger.json') as json_file:
+            crm_json = json.load(json_file)
+        
+        # Obtém o endpoint do CRM a partir do arquivo JSON do CRM
+        crm_endpoint = get_crm_endpoint(crm_json)
+        
+        if crm_endpoint:
+            # Enviando os dados para o CRM e registrando se foi enviado com sucesso ou não nos logs
+            if send_to_crm(data, crm_endpoint):
+                return {
+                    'statusCode': 200,
+                    'body': "Dados enviados com sucesso para o CRM."
+                }
+            else:
+                return {
+                    'statusCode': 500,
+                    'body': "Falha ao enviar dados para o CRM. Consulte os logs para obter mais informações."
+                }
+        else:
+            return {
+                'statusCode': 500,
+                'body': "Erro ao obter o endpoint do CRM. Consulte os logs para obter mais informações."
+            }
     except Exception as e:
-        print(f"Error reading data from S3: {e}")
+        logger.error(f"Erro ao processar o arquivo do S3: {e}")
         return {
             'statusCode': 500,
             'body': json.dumps({'error': str(e)})
